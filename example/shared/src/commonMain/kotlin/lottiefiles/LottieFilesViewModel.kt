ee3e2670390656a7f13de34d8bffb6d88501db4d
Александр Жиркевич <sasha.zhirkevich@gmail.com>
2024-08-01 00:23:42 +0300

    deploy

--- example/shared/src/commonMain/kotlin/lottiefiles/LottieFilesViewModel.kt
+++ example/shared/src/commonMain/kotlin/lottiefiles/LottieFilesViewModel.kt
@@ -72,7 +72,7 @@ internal class LottieFilesViewModel() : ViewModel() {
             }.collectLatest { (q, s, p) ->
                 try {
                     val resp = httpClient.get(
-                        "https://corsproxy.io/?https://lottiefiles.com/api/search/get-animations?query=${q.encodeURLPath()}&type=free&sort=${s.name}&page=$p"
+                        "https://corsproxy.io/?https://lottiefiles.com/api/search/get-animations?query=${q.encodeURLPath()}&type=free&sort=${s.name.lowercase()}&page=$p"
                     ).bodyAsText().let {
                         json.decodeFromString<JsonObject>(it)
                     }
