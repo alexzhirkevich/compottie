ca5b8ce5bc68625a753e14f6d5d99495e641a80d
Александр Жиркевич <sasha.zhirkevich@gmail.com>
2024-07-31 22:48:16 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 
 rm -rf ./docs
 mkdir docs
-cp -r ./example/webAppbuild/dist/compat/productionExecutable/* docs
+cp -r ./example/webApp/build/dist/compat/productionExecutable/* docs
 
 git add .
 git commit -m "deploy"
