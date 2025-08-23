50d396a15449bb8598b6733d86c566dd884af09e
Александр Жиркевич <sasha.zhirkevich@gmail.com>
2025-08-23 19:49:41 +0300

    fix compilation

--- compottie-dot/src/jvmNativeMain/kotlin/io/github/alexzhirkevich/compottie/FileSystem.jvmNative.kt
+++ compottie-dot/src/jvmNativeMain/kotlin/io/github/alexzhirkevich/compottie/FileSystem.jvmNative.kt
@@ -1,6 +1,7 @@
 package io.github.alexzhirkevich.compottie
 
 import okio.FileSystem
+import okio.SYSTEM
 
 @InternalCompottieApi
 public actual fun defaultFileSystem() : FileSystem = FileSystem.SYSTEM
