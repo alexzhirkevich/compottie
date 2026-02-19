9e19b7ff18ad4d5666d46bc8b5233d09e02cab0b
Zhirkevich Alexander Y <ZhirkevichAY@bsu.by>
2026-02-19 14:13:00 +0300

    delete wasm array converters

--- compottie-dot/src/webMain/kotlin/io/github/alexzhirkevich/compottie/ArrayConverters.web.kt
+++
@@ -1,7 +0,0 @@
-package io.github.alexzhirkevich.compottie
-
-import org.khronos.webgl.ArrayBuffer
-import org.khronos.webgl.Int8Array
-
-internal expect fun ByteArray.convertToInt8Array() : Int8Array
-internal expect fun Int8Array.convertToByteArray() : ByteArray
